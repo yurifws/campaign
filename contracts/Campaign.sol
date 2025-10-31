e1fa3d582f814f86d2353450381d4536dd7ad673
yurifws <yurifws@gmail.com>
2025-10-30 22:57:47 -0300

    <feat: ES-117> contributing to the campaign

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.19;
 contract Campaign {
     address public manager;
     uint public minimumContribution;
+    address[] public approvers;
 
     constructor(uint _minimum) {
         manager = msg.sender;
         minimumContribution = _minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
     }
 }
