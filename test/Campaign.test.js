0b26857d4c40acae0545e4a0448fd361c19f6233
yurifws <yurifws@gmail.com>
2025-11-25 23:03:14 -0300

    <feat ES-149> testing warmup

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,33 +1,43 @@
-const assert = required('assert');
+const assert = require('assert');
 const { Web3 } = require('web3');
 const ganache = require('ganache');
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require('../campaign/build/CampaignFactory.json');
-const compiledCampaign = require('../campaign/build/Campaign.json');
-const { beforeEach } = require('node:test');
+const compiledFactory = require('../build/CampaignFactory.json');
+const compiledCampaign = require('../build/Campaign.json');
 
 let accounts;
 let factory;
 let campaignAddress;
 let campaign;
 
-beforeEach( async () => {
+beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    factory = await web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({ data : compiledFactory.bytecode })
-        .send({ from: accounts[0], gas: '100000' });
+    factory = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ 
+            data: compiledFactory.evm.bytecode.object,
+            arguments: []
+        })
+        .send({ from: accounts[0], gas: '3000000' });
 
     await factory.methods.createCampaign('100').send({
         from: accounts[0],
-        gas: '1000000'
+        gas: '3000000'
     });
 
-    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-    campaign = await new web3.eth.Contract(
-        JSON.parse(compiledCampaign.interface),
+    const deployedCampaigns = await factory.methods.getDeployedCampaigns().call();
+    campaignAddress = deployedCampaigns[0];
+    
+    campaign = new web3.eth.Contract(
+        compiledCampaign.abi,
         campaignAddress
     );
-    
+});
+
+describe('Campaigns', () => {
+    it('deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
 });
