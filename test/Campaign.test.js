87c56e123b3334a0e0cf10746ca6240d46014830
yurifws <yurifws@gmail.com>
2025-11-28 16:26:27 -0300

    <feat ES-151> requiring a minimum contributions

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -55,4 +55,17 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call();
         assert(isContributor);
     });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+    });
+
 });
